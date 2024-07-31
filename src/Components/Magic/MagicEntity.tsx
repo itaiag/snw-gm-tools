e005dc4da0bb800846307ea3843e418a202d0126
itaiag <itai.agmon@gmail.com>
2024-07-31 15:35:33 +0300

    Updating magics

--- src/Components/Magic/MagicEntity.tsx
+++ src/Components/Magic/MagicEntity.tsx
@@ -1,4 +1,4 @@
-export class Magic {
+export class MagicEntity {
     constructor(public id: number, public type: string, public level: number, public name: string, public range: string, public duration: string, public description: string) { }
 
     public toString = (): string => {
